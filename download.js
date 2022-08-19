fda0e647d49aa8265662785f289cf9cc611e2881
MjBustria <86424567+MjBustria@users.noreply.github.com>
2022-08-20 02:15:33 +0800

    fixing logic in download.

--- download.js
+++ download.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     function checkLogin(){
         var UserLogin =localStorage.getItem('UserLogin');
         if(UserLogin != null){
-            document.getElementById('btnDownload').style.display = "none";
+            document.getElementById('btnDownload').style.display = "block";
         }
         else{
-            document.getElementById('btnDownload').style.display = "block";
+            document.getElementById('btnDownload').style.display = "none";
         }
     }
 });
