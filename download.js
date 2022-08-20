173a71f0eec514436dbe6f82d3644730c9472a5d
Brian Abellar <106428845+Brianjems@users.noreply.github.com>
2022-08-20 23:09:30 +0800

    final changes

--- download.js
+++ download.js
@@ -4,9 +4,15 @@ $(document).ready(function(){
         var UserLogin =localStorage.getItem('UserLogin');
         if(UserLogin != null){
             document.getElementById('btnDownload').style.display = "block";
+            document.getElementById('btnDownloadApp').style.display = "block";
+            windows.location.reload()
+            
         }
         else{
             document.getElementById('btnDownload').style.display = "none";
+            document.getElementById('btnDownloadApp').style.display = "none";
+            windows.location.reload()
+           
         }
     }
 });
